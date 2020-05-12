e1d9fd61236127d8bf94555aef11f9ebda5389d9
Stacy Gilbert <StacyGilbert7713@gmail.com>
2020-05-12 01:42:05 -0400

    Third Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,50 @@
-//let shootsTaken = document.querySelector('#teamone-numshots');
-//let goals = document.querySelector('#teamone-numgoals');
-//let goals = document.querySelector('#goals')
+let teamOneShootButton = document.querySelector ("#teamone-shoot-button")
+let teamOneNumShots = document.querySelector ("#teamone-numshots")
+let teamOneNumGoals = document.querySelector ("#teamone-numgoals")
 
-//shootsTaken.addEventListener("click", function(){
-//    console.log("+button clicked");
-//    let shootsTaken= Number(counter.innerHTML) + 1;
-//}) 
+teamOneShootButton.addEventListener ("click", function() {
+    let currentShots = teamOneNumShots.innerHTML
+    let newShots = parseInt (currentShots) + 1
+    teamOneNumShots.innerHTML = newShots
 
-
-let reset = document.querySelector ("#reset-button");
-let shoot1 = document.querySelector ("#teamone-shoot-button");
-let shoot2 = document.querySelector ("#teamtwo-shoot-button");
-
-reset.addEventListener ("click", function() {
-    console.log ("#reset-button");
-    
-})
-
-shoot1.addEventListener ("click", function() {
-    console.log ("#teamone-shoot-button");
-
-    let shoot1 = 1
-    while (shoot1 <= 10) {
-        console.log(shoot1)
-        shoot1 += 1
+    let goal = 2
+    let currentGoals = teamOneNumGoals.innerHTML
+    let newGoals = parseInt(currentGoals) + 1
+    if (Math.floor(Math.random() * 11) >= goals) {
+        teamOneNumGoals.innerHTML = newGoals
     }
-
-
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-      }
 })
 
-shoot2.addEventListener ("click", function() {
-    console.log ("#teamtwo-shoot-button");
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-      }
-    
+let teamTwoShootButton = document.querySelector("teamtwo-shoot-button")
+let teamTwoNumShots = document.querySelector("#teamtwo-numshots")
+let teamTwoNumGoals = document.querySelector("#teamtwo-numgoals")
+
+teamTwoShootButton.addEventListener ("click", function() {
+    let currentShots = teamTwoNumShots.innerHTML
+    let newShots = parseInt(currentShots) +1
+    teamTwoNumShots.innerHTML = newShots
+
+    let goal = 5
+    let currentGoals = teamTwoNumGoals.innerHTML
+    let newGoals = parseInt(currentGoals) +1
+    teamTwoNumGoals.innerHTML = newGoals
+
+    if (Math.floor(Math.random() *11) >= goal) {
+        teamTwoNumGoals.innerHTML = newGoals 
+    }
 })
+
+let resetButton = document.querySelector ("#reset-button")
+let numberOfResets = document.querySelector ("#num-resets")
+
+resetButton.addEventListener ("click", function() {
+    let currentResets = numberOfResets.innerHTML
+    let newResets = parseInt(currentResets) + 1
+    numberOfResets.innerHTML = newResets
+
+    teamOneNumShots.innerHTML = 0
+    teamTwoNumShots.innerHTML = 0
+    teamOneNumGoals.innerHTML = 0
+    teamTwoNumGoals.innerHTML = 0
+})
+
